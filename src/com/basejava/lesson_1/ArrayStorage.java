5965ac9f1f36bcd878e62e9f3356f96c58d1f2d9
mvpavlova <54674959+mvpavlova@users.noreply.github.com>
2020-06-22 20:56:10 +0300

    test2 hw1

--- src/com/basejava/lesson_1/ArrayStorage.java
+++ src/com/basejava/lesson_1/ArrayStorage.java
@@ -1,57 +1,52 @@
 package com.basejava.lesson_1;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    int size = 0;
 
     void clear() {
-        Arrays.fill(storage, null );
+        Arrays.fill(storage, 0, size,  null );
+        size = 0;
     }
 
     void save(Resume r) {
-        int count = 0;
-        while (count < storage.length) {
-            if(storage[count] == null){
-                storage[count] = r;
-                break;
-            }
-            count++;
+        if(size < storage.length) {
+            storage[size] = r;
+            size++;
         }
     }
 
-    String get(String uuid) {
-        return uuid;
+    Resume get(String uuid) {
+       for(int i = 0; i < size; i++) {
+           if(storage[i].uuid == uuid) {
+               return storage[i];
+           }
+       }
+        return null;
     }
 
     void delete(String uuid) {
-        for (int i = 0; i < storage.length; i++) {
-            if (storage[i].uuid == uuid) {
-                storage[i] = null;
-                break;
-            }
-        }
+        List<String> newStorage = new ArrayList(Arrays.asList(storage));
+        newStorage.remove(uuid);
+        storage = newStorage.toArray(new Resume[newStorage.size()]);
+        size--;
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return Arrays.copyOf(storage, size());
+        return Arrays.copyOf(storage, size);
     }
 
     int size() {
-        int count = 0;
-        while (count < storage.length) {
-            if(storage[count] == null){
-                break;
-            }
-            count++;
-        }
-
-        return count;
+        return size;
     }
 }
