4a59cab6cb2474904baeadc8ebb0b1f1a6efafd9
mvpavlova <54674959+mvpavlova@users.noreply.github.com>
2020-06-23 16:59:49 +0300

    test3 hw1

--- src/com/basejava/lesson_1/ArrayStorage.java
+++ src/com/basejava/lesson_1/ArrayStorage.java
@@ -10,36 +10,34 @@ public class ArrayStorage {
     int size = 0;
 
     void clear() {
-        Arrays.fill(storage, 0, size,  null );
+        Arrays.fill(storage, 0, size, null);
         size = 0;
     }
 
     void save(Resume r) {
-        if(size < storage.length) {
+        if (size < storage.length) {
             storage[size] = r;
             size++;
         }
     }
 
     Resume get(String uuid) {
-       for(int i = 0; i < size; i++) {
-           if(storage[i].uuid == uuid) {
-               return storage[i];
-           }
-       }
+        for (int i = 0; i < size; i++) {
+            if (storage[i].uuid == uuid) {
+                return storage[i];
+            }
+        }
         return null;
     }
 
     void delete(String uuid) {
-        int count = 0;
-        for(int i = 0; i < size ;i++) {
-            if(storage[i].uuid == uuid) {
-                count = i;
+        for (int i = 0; i < size; i++) {
+            if (storage[i].uuid == uuid) {
+                System.arraycopy(storage, i + 1, storage, i, size - 1 - i);
+                size--;
                 break;
             }
         }
-        System.arraycopy(storage, count + 1, storage, count, storage.length -1 - count);
-        size--;
     }
 
     /**
