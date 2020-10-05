7e51915ae2c6924d1ccc83c5bd100cbab000b008
mvpavlova <54674959+mvpavlova@users.noreply.github.com>
2020-10-05 22:08:20 +0300

    вызов метода toString

---
+++ src/com/basejava/webapp/MainReflection.java
@@ -0,0 +1,23 @@
+package com.basejava.webapp;
+
+import com.basejava.webapp.model.Resume;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public class MainReflection {
+    public static void main(String[] args) throws IllegalAccessException, NoSuchMethodException, InvocationTargetException {
+        Resume resume = new Resume();
+        Class<? extends Resume> resumeClass = resume.getClass(); //добавление класса
+        Field field = resume.getClass().getDeclaredFields()[0];
+        field.setAccessible(true);
+        System.out.println(field.getName());
+        System.out.println(field.get(resume));
+        field.set(resume, "new uuid");
+        Method method = resumeClass.getMethod("toString"); //вызов метода
+        Object nameResume = method.invoke(resume);
+        System.out.println(nameResume);
+        System.out.println(resume);
+    }
+}
