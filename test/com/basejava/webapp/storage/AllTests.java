ff2514d310422cfe483cccb8d94e3a2789cbf1a6
mvpavlova <54674959+mvpavlova@users.noreply.github.com>
2020-11-11 17:18:25 +0300

    add AllTests HW6 11.11

---
+++ test/com/basejava/webapp/storage/AllTests.java
@@ -0,0 +1,17 @@
+package com.basejava.webapp.storage;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        ArrayStorageTest.class,
+        ListStorageTest.class,
+        MapResumeStorageTest.class,
+        MapUuidStorageTest.class,
+        SortedArrayStorageTest.class
+})
+
+public class AllTests {
+
+}
